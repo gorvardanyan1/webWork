a58f9bcc7fb8f1e3119488144c83272ed8675e03
gorvardanyan1 <gorvardanyan1>
2023-01-13 01:44:59 +0400

    dashboard img

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navigation from './Navigation'
+import Section1 from './Section1'
 
 const Container = () => {
     return (
         <div className='container'>
             <Navigation />
+            <main>
+                <Section1 />
+            </main>
         </div>
     )
 }
