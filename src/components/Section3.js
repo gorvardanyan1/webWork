3012453bb47dec50d593f065d16cc03998dfb7a7
gorvardanyan1 <gorvardanyan1>
2023-01-14 20:17:38 +0400

    finished section3

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -2,25 +2,21 @@ import React, { useState } from 'react'
 import '../style/section3.style.scss'
 import Section3Content from './Section3Content'
 import Section3Nav from './Section3Nav'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { section3ContentData } from '../fakeData'
 const Section3 = () => {
-    const [link, setLink] = useState('')
-    function handleClick(code) {
-        console.log(section3ContentData[code]);
+    const [link, setLink] = useState('timeTracking')
+    function handleContent(data) {
+        setLink(data)
     }
     return (
         <section className='section3'>
             <h1>Save Big on All Features <br />
                 <span>no exceptions</span>
             </h1>
-            <Router>
-                <div>
-                    <Section3Nav handleClick={handleClick} />
-                    <Section3Content />
-                </div>
-            </Router>
-
+            <div className='mainDiv'>
+                <Section3Nav handleContent={handleContent} />
+                <Section3Content data={section3ContentData[link]} />
+            </div>
         </section>
     )
 }
--- src/components/Section3Content.js
+++ src/components/Section3Content.js
@@ -1,21 +1,20 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-const Section3Content = () => {
-
+import img from '../images/dashboard.png'
+import { v4 } from 'uuid'
+const Section3Content = ({ data }) => {
     return (
-        <div>
-            {/* <Routes>
-                <Route path='/' element={<div>Hi</div>} />
-                <Route path='/productivity' element={<div>Hi</div>} />
-                <Route path='/task' element={<div>blo</div>} />
-                <Route path='/comunication' element={<div>Hi</div>} />
-                <Route path='/hr' element={<div>Hi</div>} />
-                <Route path='/online' element={<div>Hi</div>} />
-            </Routes> */}
-
-            
+        <div className='section3content'>
+            <div className="contentLeft">
+                <h1>{data.header}</h1>
+                <ul>
+                    {data.advantages.map(elem => <li key={v4()}>{elem}</li>)}
+                </ul>
+                <a href={data.href}>RESERVE YOUR SEAT</a>
+            </div>
+            <div className="contentRight" >
+                <img src={img} alt="Dashboard" />
+            </div>
         </div>
     )
 }
-
 export default Section3Content
